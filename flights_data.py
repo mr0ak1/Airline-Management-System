9fb2734c07e4cd1e640d7f20302cb5c50ffcefe4
mr0ak1 <156985564+mr0ak1@users.noreply.github.com>
2024-10-24 12:05:50 +0530

    Create flights_data.py

---
+++ flights_data.py
@@ -0,0 +1,8 @@
+def get_flights():
+    return [
+        {"flight_number": "AI101", "origin": "New York", "destination": "London", "departure": "10:00 AM"},
+        {"flight_number": "AI102", "origin": "London", "destination": "New York", "departure": "2:00 PM"},
+        {"flight_number": "AI201", "origin": "Delhi", "destination": "Sydney", "departure": "6:00 AM"},
+        {"flight_number": "AI202", "origin": "Sydney", "destination": "Delhi", "departure": "4:00 PM"},
+        {"flight_number": "AI301", "origin": "New York", "destination": "Delhi", "departure": "9:00 AM"}
+    ]
